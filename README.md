ffa3e06bd96783234de1a59d3556d556f70a03b4
Austin Gill <Notgnoshi@gmail.com>
2025-04-14 18:17:45 -0500

    Add script to generate plots from session CSV files

--- README.md
+++ README.md
@@ -4,5 +4,15 @@ All d20 rolls manually recorded during the session, and processed afterwards.
 
 # TODO
 
-* [ ] Script to automate statistics and plotting for each CSV
+* [x] Script to automate statistics and plotting for each CSV
 * [ ] Generate the README with a template and a script from any sessions in `data/`
+
+# How to use
+
+Add a new CSV to the `data/` directory, in a column named `roll`. Then run the `./generate.sh`
+script on the new CSV. It require the `csvplot` and `csvstats` tools from
+https://github.com/Notgnoshi/csvizmo to be installed somewhere in your `$PATH`
+
+```sh
+./generate.sh ./data/*.csv
+```
---
+++ generate.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+set -o errexit
+set -o pipefail
+set -o nounset
+set -o noclobber
+
+RED="\033[31m"
+GREEN="\033[32m"
+BLUE="\033[34m"
+RESET="\033[0m"
+
+debug() {
+    echo -e "${BLUE}DEBUG:${RESET} $*" >&2
+}
+
+info() {
+    echo -e "${GREEN}INFO:${RESET} $*" >&2
+}
+
+error() {
+    echo -e "${RED}ERROR:${RESET} $*" >&2
+}
+
+usage() {
+    echo "Usage: $0 [--help] <CSV...>"
+    echo
+    echo "  CSV             The CSV file(s) to generate statistics for. May be repeated"
+    echo
+    echo "  --help, -h      Show this help and exit"
+}
+
+generate_plots() {
+    local -r csv="$1"
+
+    if [[ ! -f "$csv" ]]; then
+        error "CSV: '$csv' does not exist"
+        exit 1
+    fi
+    debug "Generating plots for '$csv' ..."
+
+    # NOTE: Assumes that the CSV uses 'roll' as the column name
+    csvplot --xlabel time --ylabel roll --ymin 1 --ymax 21 -y roll "$csv"
+    csvstats --discrete --min 1 --max 20 --bins 20 --histogram --column roll "$csv"
+}
+
+main() {
+    local csvs=()
+
+    while [[ $# -gt 0 ]]; do
+        case "$1" in
+        --help | -h)
+            usage
+            exit 0
+            ;;
+        -*)
+            error "Unexpected option: '$1'"
+            exit 1
+            ;;
+        *)
+            csvs+=("$1")
+            ;;
+        esac
+        shift
+    done
+
+    for csv in "${csvs[@]}"; do
+        generate_plots "$csv"
+    done
+}
+
+main "$@"
